ee2b2782ba48dc1ccd561653e1519ec1cf9b366a
Frans <franssiswanto@gmail.com>
2016-07-22 13:55:01 +0800

    refined searching algo to search existing followers

--- lib/engine/crawler.py
+++ lib/engine/crawler.py
@@ -62,19 +62,21 @@ class Engine(object):
 
 	def save(self, follower=None):
 		try:
-			assert self.db        is not None, "db is not defined."
-			assert follower       is not None, "follower is not defined."
-			assert type(follower) is dict    , "follower should be an dict."
+			assert self.db             is not None, "db is not defined."
+			assert follower            is not None, "follower is not defined."
+			assert type(follower)      is dict    , "follower should be an dict."
+			assert self.account_detail is not None, "account_detail is not defined."
 
-			# check if any duplicate data on followers
-			old_data = [follower for follower in self.db.data.find({"followers.username":follower["username"]})]
 			log      = {
 							       "type" : "follow",
 							   "username" : follower["username"],
 							"insert_date" : arrow.utcnow().datetime
 					   }
 
-			if len(old_data) == 0:
+			# check if any duplicate data on followers
+			old_data = db.data.find_one({"username":self.account_detail["username"]})
+			old_data = old_data["followers"]
+			if follower not in old_data:
 				self.db.data.update_one(
 					{"username":self.account_detail["username"]},
 					{"$push":{"followers":follower, "logs":log}}
