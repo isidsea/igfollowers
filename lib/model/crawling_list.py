049cfb3ad752193c50b214603c057b44ae602cc4
Frans <franssiswanto@gmail.com>
2016-12-13 13:08:42 +0700

    add crawlingList config

--- lib/model/crawling_list.py
+++ lib/model/crawling_list.py
@@ -1,15 +1,22 @@
 from .user import User 
 import pymongo
+import profig
 
 class CrawlingList:
 	def __init__(self):
 		self.accounts = self.get_accounts()
 
 	def get_accounts(self):
-		conn = pymongo.MongoClient("mongodb://mongo:27017/ig")
-		db   = conn["ig"]
+		config = profig.Config(os.path.join(os.getcwd(), "config", "database.cfg"))
+		config.init("crawling_list.connectionString", "mongodb://mongo:27017/ig", str)
+		config.init("crawling_list.db", "ig", str)
+		config.init("crawling_list.collection", "crawlingList", str)
+		config.sync()
 
-		docs = db.crawlingList.find({"is_active": True})
+		conn = pymongo.MongoClient(config["crawling_list.connectionString"])
+		db   = conn[config["crawling_list.db"]]
+
+		docs = db[config["crawling_list.collection"]].find({"is_active": True})
 		for doc in docs:
 			user = User()
 			user.username 	  = doc["userName"]
